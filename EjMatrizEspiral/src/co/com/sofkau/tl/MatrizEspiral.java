a752fe2ef4c55b8b2485f5db36739d8497c53beb
federzvz <85709865+federzvz@users.noreply.github.com>
2022-02-02 18:48:36 -0300

    avance

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -10,4 +10,20 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
+    public static void main(String[] args){
+        int[][] matriz = new int[8][9];
+
+        recorrerColumnaAsc(matriz, 8, 9);
+        
+    }
+
+    public static void recorrerColumnaAsc(int[][] matriz, int largoFilas, int largoColumnas){
+        for(int i=largoColumnas; i>=0; i--){
+            for(int j=largoColumnas; j>=0; j--){
+                matriz[i][j] = j*i;
+                System.out.print(matriz[i][j]);
+            }
+        }
+        System.out.print("xd");
+    }
 }
