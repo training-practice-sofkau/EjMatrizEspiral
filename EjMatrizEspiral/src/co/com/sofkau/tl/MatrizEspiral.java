f7cf0f6cd7e893231be01faa3edeb72fd224e769
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-02 18:24:37 -0300

    No creo que esto funciones

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -1,5 +1,7 @@
 package co.com.sofkau.tl;
 
+import java.util.Scanner;
+
 public class MatrizEspiral {
     // TODO: 2/02/2022 método para recorrer fila izq-der
     // TODO: 2/02/2022 método para recorrer fila der-izq
@@ -10,4 +12,34 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
+
+    public static void main(String[] args) {
+        int matriz[][] = solicitarTamaño();
+    }
+
+    /**
+     * Solicita por consola las medida de la matriz y la crea usando esos mismos parametros
+     *
+     * @return
+     */
+    public static int[][] solicitarTamaño() {
+        Scanner teclado = new Scanner(System.in);
+        int x = 0;
+        do {
+            System.out.println("Ingrese valor x, maximo 50");
+            x = teclado.nextInt();
+        } while (x > 50 && x < 1);
+
+        int y = 0;
+        do {
+            System.out.println("Ingrese valor y, maximo 50");
+            y = teclado.nextInt();
+        } while (y > 50 && y < 1);
+
+        int matriz[][] = new int[x][y];
+
+        System.out.println("Se creo exitosamente una matriz de " + x + " por " + y + ".");
+
+        return matriz;
+    }
 }
