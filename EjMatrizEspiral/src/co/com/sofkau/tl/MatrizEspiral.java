d062bc5533ba138af104d0975753ea75e476a75e
V3RSER <78446571+V3RSER@users.noreply.github.com>
2022-02-04 14:05:45 -0500

    Agregado método para crear matrices

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -1,13 +1,38 @@
 package co.com.sofkau.tl;
 
+import java.util.Scanner;
+
 public class MatrizEspiral {
     // TODO: 2/02/2022 método para recorrer fila izq-der
     // TODO: 2/02/2022 método para recorrer fila der-izq
     // TODO: 2/02/2022 método para recorrer columna desc
     // TODO: 2/02/2022 método para recorrer columna asc
     // TODO: 2/02/2022 método para solicitar tamaño de matriz a llenar
-    // TODO: 2/02/2022 método para imprimir la matriz
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
-    // parámetros: matriz, nro fila o columna, el nro en el que comienza
+    /**
+     * Crea una matriz de tamaño nxm solicitando los datos al usuario.
+     *
+     * @return int[][] Una matriz de enteros vacía.
+     */
+    public static int[][] crearMatriz() {
+        Scanner lector = new Scanner(System.in);
+        int filas;
+        int columnas;
+
+        System.out.println("Tamaño de la matriz");
+        System.out.println("- Filas:");
+        do {
+            filas = lector.nextInt();
+        } while (filas > 50 || filas < 1);
+
+        System.out.println("- Columnas:");
+        do {
+            columnas = lector.nextInt();
+        } while (columnas > 50 || columnas < 1);
+
+        lector.close();
+
+        return new int[filas][columnas];
+    }
 }
