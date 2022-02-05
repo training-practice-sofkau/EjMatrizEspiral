78e6bc4ad7d6fbed1e80b9edc3e7e7e132f626bb
Juan Manuel Reina <juanmreina97@gmail.com>
2022-02-05 18:49:12 -0500

    se creó el método de llenar la matriz de arriba a abajo

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -10,4 +10,26 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
+
+    public void llenarArribaAbajo(int[][] matriz, int numCol, int numInicial) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if ((j == numCol) && (matriz[i][j] == 0)) {
+                    matriz[i][j] = numInicial;
+                    numInicial++;
+                }
+            }
+        }
+    }
+
+    public void imprimirMatriz(int anchoMatriz, int altoMatriz, int matriz[][]) {
+        for (int i = 0; i < anchoMatriz; i++) {
+            for (int j = 0; j < altoMatriz; j++) {
+                System.out.print(matriz[i][j] + " " );
+            }
+            System.out.println("");
+        }
+    }
+
+
 }
