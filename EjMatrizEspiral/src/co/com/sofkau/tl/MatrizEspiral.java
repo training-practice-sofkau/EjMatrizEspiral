8dd1015a0ec4763b85aedb3052c63a6effc3df89
juangil991 <jcgo125@gmail.com>
2022-02-05 21:37:04 -0500

    Se añaden metodos para crear, mostrar y recorrer la matriz

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -1,5 +1,7 @@
 package co.com.sofkau.tl;
 
+import java.util.Scanner;
+
 public class MatrizEspiral {
     // TODO: 2/02/2022 método para recorrer fila izq-der
     // TODO: 2/02/2022 método para recorrer fila der-izq
@@ -10,4 +12,92 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
-}
+
+
+    public static void main(String[] args) {
+        int[][] matriz;
+        matriz = getMatriz();
+        showMatriz(avanceIzqDer(matriz, 4, 15));
+        System.out.println("matriz modificada: ");
+        showMatriz(avanceDerIzq(matriz, 6, 1));
+
+    }
+
+    /**
+     * Este metodo devuelve una matriz de ceros con las dimensiones que el usuario ingrese
+     *
+     * @return
+     */
+    public static int[][] getMatriz() {
+
+        Scanner captura = new Scanner(System.in);
+
+        System.out.println("Por favor ingrese el numero de filas que tendra la matriz: ");
+        int filas = captura.nextInt();
+
+        System.out.println("Por favor ingrese el numero de columnas que tendra la matriz: ");
+        int columnas = captura.nextInt();
+
+        int[][] matriz;
+        matriz = new int[filas][columnas];
+
+        return matriz;
+    }
+
+    /**
+     * Este metodo recibe una matriz y la imprime por cosola
+     *
+     * @param matriz matriz a imprimir
+     */
+    public static void showMatriz(int[][] matriz) {
+
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(" " + matriz[i][j] + ",");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+
+    /**
+     * Este metodo recibe una matriz y la recorre de izquierda a derecha modificando sus valores
+     *
+     * @param matriz      matriz a modificar
+     * @param filaInicial fila a partir de la cual se modifican los datos
+     * @param numInicial  numero inicial, aumenta ascendentemente mientras se recorre la matriz
+     * @return retorna la matriz modificada
+     */
+    public static int[][] avanceIzqDer(int[][] matriz, int filaInicial, int numInicial) {
+
+        int columnas = matriz[0].length;
+        for (int i = 0; i < columnas; i++) {
+            if (matriz[filaInicial - 1][i] == 0) {
+                matriz[filaInicial - 1][i] = numInicial;
+                numInicial++;
+            }
+        }
+        return matriz;
+    }
+
+    /**
+     * Este metodo recibe una matriz y la recorre de derecha a izquierda modificando sus valores
+     *
+     * @param matriz      matriz a modificar
+     * @param filaInicial fila a partir de la cual se modifican los datos
+     * @param numInicial  numero inicial, aumenta ascendentemente mientras se recorre la matriz
+     * @return retorna la matriz modificada
+     */
+    public static int[][] avanceDerIzq(int[][] matriz, int filaInicial, int numInicial) {
+        int columnas = matriz[0].length;
+        for (int i = columnas - 1; i >= 0; i--) {
+            if (matriz[filaInicial - 1][i] == 0) {
+                matriz[filaInicial - 1][i] = numInicial;
+                numInicial++;
+            }
+        }
+        return matriz;
+    }
+} 
