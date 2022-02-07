76e19ea5398695c2a2f8ff9442daeb9cf408ebb1
Alvaro Mena <alvaro16a@gmail.com>
2022-02-07 13:46:46 -0500

    Se creo el metodo getMatriz

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -1,5 +1,7 @@
 package co.com.sofkau.tl;
 
+import java.util.Scanner;
+
 public class MatrizEspiral {
     // TODO: 2/02/2022 método para recorrer fila izq-der
     // TODO: 2/02/2022 método para recorrer fila der-izq
@@ -10,4 +12,25 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
+
+    /**
+     * Este metodo devuelve una matriz de ceros con las dimensiones que el usuario ingrese
+     * @return
+     */
+    public static int[][] getMatriz(){
+
+        Scanner captura = new Scanner(System.in);
+
+        System.out.println("Por favor ingrese el numero de filas que tendra la matriz: ");
+        int filas = captura.nextInt();
+
+        System.out.println("Por favor ingrese el numero de columnas que tendra la matriz: ");
+        int columnas = captura.nextInt();
+
+        int [][] matriz;
+        matriz = new int[filas][columnas];
+
+        return matriz;
+    }
+
 }
